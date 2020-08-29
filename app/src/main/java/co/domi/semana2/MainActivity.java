861cd32c30f40fc3cce079a8e77713cd215368e7
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2020-08-28 19:33:00 -0500

    Second Commit

--- app/src/main/java/co/domi/semana2/MainActivity.java
+++ app/src/main/java/co/domi/semana2/MainActivity.java
@@ -3,20 +3,31 @@ package co.domi.semana2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import static java.lang.Math.random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView Pregunta;
     private EditText Respuesta;
     private Button Aceptar;
-    private int Puntos = 0;
-    private ArrayList<Pregunta> preguntas;
+    private Button Intentarlo;
+    private TextView Puntos;
+    private TextView Tiempo;
+    private int punticos;
+    private Pregunta preguntas;
+    private Random random;
+    private int a;
+    private Boolean PrenderContador;
+    private int tiempo;
 
 
     @Override
@@ -27,14 +38,73 @@ public class MainActivity extends AppCompatActivity {
         Pregunta = findViewById(R.id.Pregunta);
         Respuesta = findViewById(R.id.Respuesta);
         Aceptar = findViewById(R.id.Aceptar);
-        preguntas = new ArrayList<Pregunta>();
+        Intentarlo = findViewById(R.id.Intentar);
+        Puntos = findViewById(R.id.Puntos);
+        Tiempo = findViewById(R.id.Tiempo);
+        PrenderContador = true;
+        random = new Random();
+        tiempo = 20;
 
-        Aceptar.setOnClickListener(
+        PreguntasSiguiente();
+        Temporizador();
+
+        Respuesta.setOnClickListener(
                 (v) -> {
-                    String respuesta = Respuesta.getText().toString();
-                    Puntos = Puntos + 15;
+                    String MiRespuesta = Respuesta.getText().toString();
+                        if (MiRespuesta.equals(Integer.toString(preguntas.getRespuesta()))) {
+                            punticos += 15;
+                            PreguntasSiguiente();
+                            Respuesta.setText("");
+                        } else {
+                            punticos -= 10;
+                        }
+                    Puntos.setText("puntajes:"+punticos);
                 }
-                );
+        );
+
+        Intentarlo.setOnClickListener(
+                (v) -> {
+                    PrenderContador = true;
+
+                    punticos = 0;
+                    Puntos.setText("puntajes:"+punticos);
+                    Tiempo.setText(" " + tiempo);
+                    Intentarlo.setVisibility(View.GONE);
+                }
+        );
+    }
+
+    public void PreguntasSiguiente(){
+        int numeroRandomUno = (int) Math.floor((Math.random() * 10 + 1));
+        int numeroRandomDos = (int) Math.floor((Math.random() * 10 + 1));
+        int operacion = (int) Math.floor(Math.random() * 4 + 1);
+        preguntas = new Pregunta(numeroRandomUno,numeroRandomDos,operacion);
+        preguntas.generadorPregunta();
+        Pregunta.setText(preguntas.devolverPregunta()+" ");
 
     }
+
+    public void Temporizador(){
+        new Thread(
+                () -> {
+                    while(PrenderContador){
+                        tiempo--;
+                        runOnUiThread(() ->Tiempo.setText(" " + tiempo));
+                        if(tiempo <= 0){
+                            PrenderContador = false;
+                            tiempo = 20;
+                            runOnUiThread(() ->Intentarlo.setVisibility(View.VISIBLE));
+                        }
+                        try {
+                           Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).start();
+    }
+
+
+
 }
